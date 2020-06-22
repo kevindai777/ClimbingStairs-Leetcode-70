d4940655ad0b853f7229e6db70ba120f5f1c6b71
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-22 08:09:13 -0400

    edits to explanation

--- climbingstairs.js
+++ climbingstairs.js
@@ -4,15 +4,15 @@
 let n = 3
 
 
-
-//O(n) solution via fibonacci sequence
+//O(n) solution via dynamic programming and math
 
 //We realize that as the number of stairs grows, the number of ways to climb
 //is the sum of the previous two terms
 
-//For 0 and 1 stairs
+//Solutions for 0 and 1 stairs respectfully
 let arr = [0, 1]
 
+//Get the sum of the previous two terms and expand array
 for (let i = 2; i < n + 2; i++) {
     arr[i] = arr[i - 1] + arr[i - 2]
 }
